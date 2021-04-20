840a3bab94ee6caf2b395dec287364639ffac37c
metoonhathung <nhathungmetoo@gmail.com>
2021-04-20 12:08:48 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-00815f254c04bb79afa5.js"
+    "url": "webpack-runtime-ddf8e0a5a1284d505266.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "96fa4a6225efbbddb2539fb6e505864c"
+    "revision": "4c0cde61bfb658cdc06f205dd93059f8"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
